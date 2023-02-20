f1c30e2ddf9eff53476ecb156204a4af3bf29591
Bralen Sundquist <bralencsundquist@hotmail.com>
2023-02-20 16:16:58 -0600

    removed nextauth and google oauth2

--- components/Especialistas/ProfileCard/Cita.jsx
+++ components/Especialistas/ProfileCard/Cita.jsx
@@ -2,50 +2,15 @@ import { useState } from "react";
 import { useForm } from "react-hook-form";
 import { checkout } from "./checkout";
 
-// import { google } from "googleapis";
-
 // MUI
 import TextField from "@mui/material/TextField";
 import { LocalizationProvider } from "@mui/x-date-pickers/LocalizationProvider";
 import { AdapterDateFns } from "@mui/x-date-pickers/AdapterDateFns";
 import { DateTimePicker } from "@mui/x-date-pickers/DateTimePicker";
-import LoginTest from "@/components/loginTest/LoginTest";
 
 export default function Cita({ stripePriceID }) {
   //   console.log(stripePriceID);
   const { register, handleSubmit } = useForm();
-
-  const createEvent = async () => {
-    // Load the Google Calendar API
-    const calendar = google.calendar({
-      version: "v3",
-      auth: session.accessToken,
-    });
-
-    // Create a new event
-    const event = {
-      summary: "Test Event",
-      location: "New York",
-      description: "This is a test event",
-      start: {
-        dateTime: "2023-03-01T09:00:00-07:00",
-        timeZone: "America/Los_Angeles",
-      },
-      end: {
-        dateTime: "2023-03-01T17:00:00-07:00",
-        timeZone: "America/Los_Angeles",
-      },
-      reminders: {
-        useDefault: true,
-      },
-    };
-
-    // Insert the new event
-    await calendar.events.insert({
-      calendarId: "primary",
-      resource: event,
-    });
-  };
 
   const [value, setValue] = useState(new Date());
 
@@ -126,22 +91,20 @@ export default function Cita({ stripePriceID }) {
               type="text"
             />
           </div>
-          <LoginTest />
           <button
-            onClick={createEvent}
-            // onClick={() =>
-            //   checkout({
-            //     lineItems: [
-            //       {
-            //         price: `${stripePriceID}`,
-            //         quantity: 1,
-            //       },
-            //     ],
-            //   })
-            // }
-            // type="submit"
-            // role="link"
-            // className="py-5 px-10 rounded-full text-white bg-[#f28482] font-bold"
+          // onClick={() =>
+          //   checkout({
+          //     lineItems: [
+          //       {
+          //         price: `${stripePriceID}`,
+          //         quantity: 1,
+          //       },
+          //     ],
+          //   })
+          // }
+          // type="submit"
+          // role="link"
+          // className="py-5 px-10 rounded-full text-white bg-[#f28482] font-bold"
           >
             Pagar Ahora
           </button>
