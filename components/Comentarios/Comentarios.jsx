829cab180ddcd001c3973d1527bc2413f47e3960
Bralen Sundquist <bralencsundquist@hotmail.com>
2023-03-11 12:01:05 -0600

    added third screen size for smaller labtops/ipads

--- components/Comentarios/Comentarios.jsx
+++ components/Comentarios/Comentarios.jsx
@@ -1,66 +1,41 @@
-"use client";
-
 import ComentarioCard from "./ComentarioCard";
-import { motion } from "framer-motion";
+
+import MotionTitle from "../Motion/MotionTitle";
+import MotionDiv from "../Motion/MotionDiv";
 
 export default function Comentarios({ comentarios }) {
   return (
     // need relative here with h-screen
     <div className=" h-screen flex flex-col relative mx-auto items-center  md:flex-row max-w-7xl px-8 md:justify-center">
-      <div className="absolute text-center top-[10px] md:top-[25px] flex flex-row">
-        <motion.h3
-          initial={{
-            y: -25,
-            opacity: 0,
-          }}
-          whileInView={{
-            y: 0,
-            opacity: 1,
-          }}
-          transition={{
-            duration: 1.2,
-          }}
-          className="uppercase tracking-[15px] md:tracking-[20px] text-[#f28482] md:text-2xl"
-        >
-          Comentarios
-        </motion.h3>
+      <div className="absolute flex top-[15px] md:top-[10px]  ">
+        <div className="flex flex-col md:flex-row items-center">
+          <MotionDiv
+            x={25}
+            y={25}
+            duration={1.2}
+            useAnimate={false}
+            className=" md:w-[2px] md:h-[32px] bg-[#f28482] mr-[15px]"
+          />
+          <MotionTitle
+            y={-25}
+            duration={1.2}
+            className="uppercase tracking-[15px] md:tracking-[20px] text-[#f28482] md:text-2xl"
+          >
+            Comentarios
+          </MotionTitle>
 
-        <motion.div
-          initial={{
-            opacity: 0,
-            x: 25,
-            y: 25,
-          }}
-          whileInView={{
-            opacity: 1,
-            x: 0,
-            y: 0,
-          }}
-          transition={{
-            duration: 1.2,
-          }}
-          className=" absolute right-[400px] md:w-[2px] md:h-[32px] bg-[#f28482]"
-        />
-        <motion.div
-          initial={{
-            opacity: 0,
-            x: -25,
-            y: -25,
-          }}
-          whileInView={{
-            opacity: 1,
-            x: 0,
-            y: 0,
-          }}
-          transition={{
-            duration: 1.2,
-          }}
-          className=" absolute right-0 md:w-[2px] md:h-[32px] bg-[#f28482]"
-        />
+          <MotionDiv
+            x={-25}
+            y={-25}
+            duration={1.2}
+            useAnimate={false}
+            className=" md:w-[2px] md:h-[32px] bg-[#f28482] mr-[15px]"
+          />
+        </div>
       </div>
 
       <div
-        className="absolute top-[50px] md:top-[100px] flex flex-col h-[550px] md:h-[650px] overflow-x-scroll snap-x snap-mandatory
+        className="relative top-[50px] md:-top-[120px] flex flex-col h-[475px] overflow-x-scroll snap-x snap-mandatory
         md:scrollbar scrollbar-track-[#f7cad0] scrollbar-thumb-[#f28482]"
       >
         <div className="md:grid grid-cols-4 gap-4 md:grid-cols-3 space-y-3 md:space-y-0">
--- components/Productos/Productos.jsx
+++ components/Productos/Productos.jsx
@@ -1,15 +1,13 @@
-"use client";
 import MotionTitle from "../Motion/MotionTitle";
 import MotionDiv from "../Motion/MotionDiv";
 
 import ProductoCard from "./ProductoCard";
-import { motion } from "framer-motion";
 
 export default function Productos({ productos }) {
   return (
     // need relative here with h-screen
     <div className=" h-screen flex flex-col relative mx-auto items-center max-w-7xl px-8 md:justify-center">
-      <div className="absolute flex md:top-[10px]  ">
+      <div className="absolute flex top-[15px] md:top-[10px]  ">
         <div className="flex flex-col md:flex-row items-center">
           <MotionDiv
             x={25}
@@ -42,11 +40,11 @@ export default function Productos({ productos }) {
         </div>
       </div>
 
-      <h2 className="relative md:-top-[150px]  flex text-[#f28482] text-[18px]">
+      <h2 className="relative top-[50px] md:-top-[150px]  flex text-[#f28482] text-[18px]">
         Productos Ariix
       </h2>
       <div
-        className="relative md:-top-[120px] flex flex-col h-[475px] overflow-x-scroll snap-x snap-mandatory
+        className="relative top-[50px] md:-top-[120px] flex flex-col h-[475px] overflow-x-scroll snap-x snap-mandatory
         md:scrollbar scrollbar-track-[#f7cad0] scrollbar-thumb-[#f28482]"
       >
         <div className="md:grid gap-4 grid-cols-4 space-y-3 md:space-y-0">
